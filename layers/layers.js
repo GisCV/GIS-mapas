733b096599327b45d56d7c6e50965ee65e88ebf8
GisCV <candelavillanueva@gmail.com>
2024-11-18 18:04:45 -0300

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -11,87 +11,48 @@ var wms_layers = [];
                 url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
             })
         });
-var lyr_ndvi1011_1 = new ol.layer.Image({
+var lyr_ndvi1511hs_1 = new ol.layer.Image({
                             opacity: 1,
-                            title: "ndvi 10.11",
+                            title: "ndvi 15.11 hs",
                             
                             
                             source: new ol.source.ImageStatic({
-                               url: "./layers/ndvi1011_1.png",
+                               url: "./layers/ndvi1511hs_1.png",
     attributions: ' ',
                                 projection: 'EPSG:3857',
                                 alwaysInRange: true,
-                                imageExtent: [-6399053.429925, -3879185.809308, -6389439.125122, -3866683.585850]
+                                imageExtent: [-6397869.552199, -3876959.440147, -6395346.751731, -3874981.930480]
                             })
                         });
-var lyr_NDVI511_2 = new ol.layer.Image({
-                            opacity: 1,
-                            title: "NDVI 5.11",
-                            
-                            
-                            source: new ol.source.ImageStatic({
-                               url: "./layers/NDVI511_2.png",
-    attributions: ' ',
-                                projection: 'EPSG:3857',
-                                alwaysInRange: true,
-                                imageExtent: [-6399052.873329, -3879145.536477, -6389486.076290, -3866722.834994]
-                            })
-                        });
-var lyr_ndvi1311_3 = new ol.layer.Image({
-                            opacity: 1,
-                            title: "ndvi 13.11",
-                            
-                            
-                            source: new ol.source.ImageStatic({
-                               url: "./layers/ndvi1311_3.png",
-    attributions: ' ',
-                                projection: 'EPSG:3857',
-                                alwaysInRange: true,
-                                imageExtent: [-6399053.419156, -3879185.775389, -6389439.118324, -3866683.598846]
-                            })
-                        });
-var lyr_ndvi1511_4 = new ol.layer.Image({
-                            opacity: 1,
-                            title: "ndvi 15.11",
-                            
-                            
-                            source: new ol.source.ImageStatic({
-                               url: "./layers/ndvi1511_4.png",
-    attributions: ' ',
-                                projection: 'EPSG:3857',
-                                alwaysInRange: true,
-                                imageExtent: [-6399053.419156, -3879185.775389, -6389439.118324, -3866683.598846]
-                            })
-                        });
-var format_oliver_5 = new ol.format.GeoJSON();
-var features_oliver_5 = format_oliver_5.readFeatures(json_oliver_5, 
+var format_HenrySchrder_2 = new ol.format.GeoJSON();
+var features_HenrySchrder_2 = format_HenrySchrder_2.readFeatures(json_HenrySchrder_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_oliver_5 = new ol.source.Vector({
+var jsonSource_HenrySchrder_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_oliver_5.addFeatures(features_oliver_5);
-var lyr_oliver_5 = new ol.layer.Vector({
+jsonSource_HenrySchrder_2.addFeatures(features_HenrySchrder_2);
+var lyr_HenrySchrder_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_oliver_5, 
-                style: style_oliver_5,
-                popuplayertitle: "oliver",
+                source:jsonSource_HenrySchrder_2, 
+                style: style_HenrySchrder_2,
+                popuplayertitle: "Henry Schröder",
                 interactive: true,
-                title: '<img src="styles/legend/oliver_5.png" /> oliver'
+                title: '<img src="styles/legend/HenrySchrder_2.png" /> Henry Schröder'
             });
-var group_Oliver = new ol.layer.Group({
-                                layers: [lyr_ndvi1011_1,lyr_NDVI511_2,lyr_ndvi1311_3,lyr_ndvi1511_4,lyr_oliver_5,],
+var group_HS = new ol.layer.Group({
+                                layers: [lyr_ndvi1511hs_1,lyr_HenrySchrder_2,],
                                 fold: "open",
-                                title: "Oliver"});
+                                title: "H.S"});
 var group_group1 = new ol.layer.Group({
                                 layers: [lyr_googlesatelite_0,],
                                 fold: "open",
                                 title: "group1"});
 
-lyr_googlesatelite_0.setVisible(true);lyr_ndvi1011_1.setVisible(true);lyr_NDVI511_2.setVisible(true);lyr_ndvi1311_3.setVisible(true);lyr_ndvi1511_4.setVisible(true);lyr_oliver_5.setVisible(true);
-var layersList = [group_group1,group_Oliver];
-lyr_oliver_5.set('fieldAliases', {'Especie': 'Especie', 'id': 'id', 'Potrero': 'Potrero', 'Campo': 'Campo', 'Area': 'Area', '5.11.24_TC': '5.11.24_TC', '10.11.24_TC': '10.11.24_TC', '13.11.24_TC': '13.11.24_TC', '15.11.24_TC': '15.11.24_TC', });
-lyr_oliver_5.set('fieldImages', {'Especie': 'TextEdit', 'id': 'TextEdit', 'Potrero': 'TextEdit', 'Campo': 'TextEdit', 'Area': '', '5.11.24_TC': 'TextEdit', '10.11.24_TC': 'TextEdit', '13.11.24_TC': 'TextEdit', '15.11.24_TC': 'TextEdit', });
-lyr_oliver_5.set('fieldLabels', {'Especie': 'header label - always visible', 'id': 'hidden field', 'Potrero': 'header label - always visible', 'Campo': 'inline label - visible with data', 'Area': 'header label - always visible', '5.11.24_TC': 'header label - always visible', '10.11.24_TC': 'header label - always visible', '13.11.24_TC': 'header label - always visible', '15.11.24_TC': 'header label - always visible', });
-lyr_oliver_5.on('precompose', function(evt) {
+lyr_googlesatelite_0.setVisible(true);lyr_ndvi1511hs_1.setVisible(true);lyr_HenrySchrder_2.setVisible(true);
+var layersList = [group_group1,group_HS];
+lyr_HenrySchrder_2.set('fieldAliases', {'id': 'id', 'Potrero': 'Potrero', 'Especie': 'Especie', 'Area': 'Area', 'TC 15.11.24TC': 'TC 15.11.24TC', });
+lyr_HenrySchrder_2.set('fieldImages', {'id': 'TextEdit', 'Potrero': 'TextEdit', 'Especie': 'TextEdit', 'Area': 'TextEdit', 'TC 15.11.24TC': 'TextEdit', });
+lyr_HenrySchrder_2.set('fieldLabels', {'id': 'hidden field', 'Potrero': 'header label - always visible', 'Especie': 'header label - always visible', 'Area': 'header label - always visible', 'TC 15.11.24TC': 'header label - always visible', });
+lyr_HenrySchrder_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
