ef513974bba63e1604a20c7ba384f53c994cffa9
GisCV <candelavillanueva@gmail.com>
2024-12-11 19:04:09 -0300

    Add files via upload

---
+++ GisWebSM101224/layers/ResumenTCpromediokgMShad_5.js
@@ -0,0 +1,10 @@
+var json_ResumenTCpromediokgMShad_5 = {
+"type": "FeatureCollection",
+"name": "ResumenTCpromediokgMShad_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Cría 5.12": "Sta. Beatriz: 24.6", "Recría 5.": "Sta. Beatriz: 35.9", "Cría 10.1": "Sta. Beatriz: 24.8", "Recría 10": "Sta. Beatriz: 39.9" }, "geometry": { "type": "Point", "coordinates": [ -57.609624154093311, -33.400074354545488 ] } },
+{ "type": "Feature", "properties": { "Cría 5.12": "Isletas: 20.1", "Recría 5.": "Isletas: 22.7", "Cría 10.1": "Isletas: 20.2", "Recría 10": "Isletas: 24.3" }, "geometry": { "type": "Point", "coordinates": [ -57.563350906801112, -33.447423814260468 ] } },
+{ "type": "Feature", "properties": { "Cría 5.12": "Casco: 22.3", "Recría 5.": "Casco: 20.8", "Cría 10.1": "Casco: 22.6", "Recría 10": null }, "geometry": { "type": "Point", "coordinates": [ -57.607721648927999, -33.498375400366818 ] } }
+]
+}
