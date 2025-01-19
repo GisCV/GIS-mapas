e93b55d75bb8ab653b0731725394f3d19e185441
GisCV <candelavillanueva@gmail.com>
2025-01-19 19:38:27 -0300

    Add files via upload

---
+++ GisWebSM170125/layers/ResumenTCpromediokgMShad_17.js
@@ -0,0 +1,10 @@
+var json_ResumenTCpromediokgMShad_17 = {
+"type": "FeatureCollection",
+"name": "ResumenTCpromediokgMShad_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Cría 5.12": "Sta. Beatriz: 24.6", "Recría 5.": "35.9", "Cría 10.1": "24.8", "Recría 10": "39.9", "Cría 30.1": "13.8", "Recría301": "17.7", "Cría4.1.2": "14.3", "Recría4.1": "13.6", "Cría9.1": "9.6", "Recría9.1": "7.5", "Cría11.1": "8.9", "Recría11.": "6.1", "Cría14.1": "8.4", "Recría14.": "5.3", "Cría17.01": "7.5", "Recría17.": "4.0" }, "geometry": { "type": "Point", "coordinates": [ -57.609624154093311, -33.400074354545488 ] } },
+{ "type": "Feature", "properties": { "Cría 5.12": "Isletas: 20.1", "Recría 5.": "22.7", "Cría 10.1": "20.2", "Recría 10": "24.3", "Cría 30.1": "13.7", "Recría301": "14.7", "Cría4.1.2": "13.7", "Recría4.1": "12.1", "Cría9.1": "9.8", "Recría9.1": "6.9", "Cría11.1": "9.4", "Recría11.": "5.5", "Cría14.1": "9", "Recría14.": "4.5", "Cría17.01": "8.4", "Recría17.": "3" }, "geometry": { "type": "Point", "coordinates": [ -57.563350906801112, -33.447423814260468 ] } },
+{ "type": "Feature", "properties": { "Cría 5.12": "Casco: 22.3", "Recría 5.": "20.8", "Cría 10.1": "22.6", "Recría 10": null, "Cría 30.1": "16.2", "Recría301": "17", "Cría4.1.2": "14.4", "Recría4.1": "14.2", "Cría9.1": "9.8", "Recría9.1": "8.8", "Cría11.1": "9.3", "Recría11.": "7.8", "Cría14.1": "8.7", "Recría14.": "7.2", "Cría17.01": "7.9", "Recría17.": "6" }, "geometry": { "type": "Point", "coordinates": [ -57.607721648927999, -33.498375400366818 ] } }
+]
+}
