89d96191bc0d2ff637087e0cdf7a484bed5bb986
GisCV <candelavillanueva@gmail.com>
2025-02-03 11:43:39 -0300

    Add files via upload

---
+++ GisWeb010225SM/layers/ResumenTCpromediokgMShad_21.js
@@ -0,0 +1,10 @@
+var json_ResumenTCpromediokgMShad_21 = {
+"type": "FeatureCollection",
+"name": "ResumenTCpromediokgMShad_21",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Cría11.1": "8.9", "Recría11.": "6.1", "Cría14.1": "8.4", "Recría14.": "5.3", "Cría17.01": "7.5", "Recría17.": "4.0", "Cría22.01": "10.1", "Recría22.": "6.3", "Cría01.02": "18.4", "Recría01.": "17.3" }, "geometry": { "type": "Point", "coordinates": [ -57.609624154093311, -33.400074354545495 ] } },
+{ "type": "Feature", "properties": { "Cría11.1": "9.4", "Recría11.": "5.5", "Cría14.1": "9", "Recría14.": "4.5", "Cría17.01": "8.4", "Recría17.": "3", "Cría22.01": "11.2", "Recría22.": "4.5", "Cría01.02": "20.2", "Recría01.": "9.6" }, "geometry": { "type": "Point", "coordinates": [ -57.563350906801112, -33.447423814260468 ] } },
+{ "type": "Feature", "properties": { "Cría11.1": "9.3", "Recría11.": "7.8", "Cría14.1": "8.7", "Recría14.": "7.2", "Cría17.01": "7.9", "Recría17.": "6", "Cría22.01": "10.3", "Recría22.": "8.3", "Cría01.02": "15.3", "Recría01.": "20.1" }, "geometry": { "type": "Point", "coordinates": [ -57.607721648927999, -33.498375400366811 ] } }
+]
+}
