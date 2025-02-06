44b5135355cab953165ced510826374b58377e53
GisCV <candelavillanueva@gmail.com>
2025-02-06 08:55:23 -0300

    Add files via upload

---
+++ GisWebSM030225/layers/ResumenTCpromediokgMShad_23.js
@@ -0,0 +1,10 @@
+var json_ResumenTCpromediokgMShad_23 = {
+"type": "FeatureCollection",
+"name": "ResumenTCpromediokgMShad_23",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "SemanaCrí": "16.7", "SemanaRecr": "15.6", "EneroCría": "10.5", "EneroRecr�": "7.6" }, "geometry": { "type": "Point", "coordinates": [ -57.609624154093311, -33.400074354545495 ] } },
+{ "type": "Feature", "properties": { "SemanaCrí": "18.3", "SemanaRecr": "8.6", "EneroCría": "10.9", "EneroRecr�": "6.5" }, "geometry": { "type": "Point", "coordinates": [ -57.563350906801112, -33.447423814260468 ] } },
+{ "type": "Feature", "properties": { "SemanaCrí": "13.4", "SemanaRecr": "18.3", "EneroCría": "10.8", "EneroRecr�": "9.3" }, "geometry": { "type": "Point", "coordinates": [ -57.607721648927999, -33.498375400366811 ] } }
+]
+}
