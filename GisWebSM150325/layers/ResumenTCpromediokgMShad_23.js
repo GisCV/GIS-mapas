3f9663da956a447a02b19d1bc8a5e9817d0c6b80
GisCV <candelavillanueva@gmail.com>
2025-03-16 21:02:36 -0300

    Add files via upload

---
+++ GisWebSM150325/layers/ResumenTCpromediokgMShad_23.js
@@ -0,0 +1,10 @@
+var json_ResumenTCpromediokgMShad_23 = {
+"type": "FeatureCollection",
+"name": "ResumenTCpromediokgMShad_23",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "SemanaCrí": "37.4", "SemanaRecr": "44.6", "FebreroCr�": "22.3", "FebreroRec": "21.5" }, "geometry": { "type": "Point", "coordinates": [ -57.609624154093311, -33.400074354545495 ] } },
+{ "type": "Feature", "properties": { "SemanaCrí": "38.2", "SemanaRecr": "34.2", "FebreroCr�": "23.7", "FebreroRec": "14.0" }, "geometry": { "type": "Point", "coordinates": [ -57.563350906801112, -33.447423814260468 ] } },
+{ "type": "Feature", "properties": { "SemanaCrí": "35.6", "SemanaRecr": "60.3", "FebreroCr�": "19.0", "FebreroRec": "26.8" }, "geometry": { "type": "Point", "coordinates": [ -57.607721648927999, -33.498375400366811 ] } }
+]
+}
